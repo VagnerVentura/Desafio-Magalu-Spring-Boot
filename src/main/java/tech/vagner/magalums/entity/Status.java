1bfd36e6b7d7c19f27bc9691fec741d75e077956
vagner ventura <vagnerventura@hotmail.com>
2024-10-26 14:42:56 -0300

    Criando entidades e seus relacionamentos

--- src/main/java/tech/vagner/magalums/entity/Status.java
+++ src/main/java/tech/vagner/magalums/entity/Status.java
@@ -1,5 +1,63 @@
 package tech.vagner.magalums.entity;
 
+import org.springframework.context.annotation.Description;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_status")
 public class Status {
 
+	@Id
+	private Long statusId;
+	
+	private String description;
+
+	public Status() {}
+	
+	public Status(Long statusId, String description) {
+		this.setStatusId(statusId);
+		this.setDescription(description);
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(Long statusId) {
+		this.statusId = statusId;
+	}
+	
+	public enum Values {
+		
+		PENDING(1L,"pending"),
+		SUCCESS(2L, "success"),
+		ERROR(3L, "error"),
+		CANCELED(4L, "canceled");
+
+		
+		private Long id;
+		private String description;
+		
+		Values(long id, String description) {
+			this.id = id;
+			this.description = description;
+		}
+		
+		public Status toStatus() {
+			return new Status(id, description);
+		}
+		
+	}
+	
 }
